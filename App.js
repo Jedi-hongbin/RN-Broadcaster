2a299f6c7ebea097691069b1359b59761d5d349f
Hongbin <2218176087@qq.com>
2021-01-09 15:53:51 +0800

    test

--- App.js
+++ App.js
@@ -1,42 +1,68 @@
 import React, {useRef} from 'react';
-import {SafeAreaView, StatusBar, Text, Button} from 'react-native';
-import RNBambuserBroadcaster from 'react-native-bambuser-broadcaster';
+import {
+  SafeAreaView,
+  StatusBar,
+  Button,
+  StyleSheet,
+  Dimensions,
+  View,
+} from 'react-native';
+import {NodeCameraView} from 'react-native-nodemediaclient';
 
+const {height, width} = Dimensions.get('window');
 const App: () => React$Node = () => {
-  const Broadcaster = useRef(null);
+  const NodeCamera = useRef(null);
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <Text>hello</Text>
-        <RNBambuserBroadcaster
-          style={{width: '100%', height: 200, backgroundColor: '#000'}}
-          ref={Broadcaster}
-          title="test title"
-          Aspect={{width: 1, height: 2}}
-          onStartBroadcastNotReady={(e) => {
-            console.log('onStartBroadcastNotReady:', e);
+      <SafeAreaView style={{flex: 1}}>
+        <NodeCameraView
+          ref={NodeCamera}
+          style={[StyleSheet.absoluteFill, {height}]}
+          outputUrl={
+            'rtmp://125087.livepush.myqcloud.com/live/nodemedia?txSecret=91b92743eb120a4c6f33468512a08857&txTime=5FFD4A2D'
+          }
+          camera={{cameraId: 1, cameraFrontMirror: true}}
+          audio={{bitrate: 32000, profile: 1, samplerate: 44100}}
+          video={{
+            preset: 2,
+            bitrate: 400000,
+            profile: 1,
+            fps: 30, //[15,20,24,30]
+            videoFrontMirror: false,
           }}
-          onBroadcastStarted={(e) => {
-            console.log('onBroadcastStarted:', e);
-          }}
-          onBroadcastError={(errorCode, errorMessage) => {
-            console.log('onBroadcastError:', errorCode, errorMessage);
-          }}
-          applicationId={'7iR4RifZUagPInJu2t4aMg'}
+          autopreview={true}
+          denoise={true}
+          smoothSkinLevel={5}
+          onStatus={(e) => console.log('onStatueChange', e)}
         />
-        <Button
-          title="start"
-          onPress={() => {
-            Broadcaster?.current.startBroadcast();
-          }}
-        />
-        <Button
-          title="stop"
-          onPress={() => {
-            Broadcaster?.current.stopBroadcast();
-          }}
-        />
+        <View
+          style={{
+            position: 'absolute',
+            bottom: 0,
+            width,
+            justifyContent: 'space-around',
+            flexDirection: 'row',
+          }}>
+          <Button
+            title="start"
+            onPress={() => {
+              NodeCamera?.current.start();
+            }}
+          />
+          <Button
+            title="stop"
+            onPress={() => {
+              NodeCamera?.current.stop();
+            }}
+          />
+          <Button
+            title="switchCamera"
+            onPress={() => {
+              NodeCamera?.current.switchCamera();
+            }}
+          />
+        </View>
       </SafeAreaView>
     </>
   );
